3c22ac6c43c6e70660aa497fdeab7b8ab8e695b4
snailoff <snailoff@minimalworks.io>
2022-02-25 09:56:13 +0900

    [feat] lib version

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject knot-backend "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [environ "1.2.0"]
                  [clj-jgit "1.0.2"]
                  [org.immutant/scheduling "2.1.10"]
-                 [org.clojure/java.jdbc "0.7.11"]
-                 [com.github.seancorfield/honeysql "2.0.0-rc2"]
-                 [org.postgresql/postgresql "42.2.14"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [com.github.seancorfield/honeysql "2.2.861"]
+                 [org.postgresql/postgresql "42.3.2"]
                  [metosin/reitit "0.5.15"]
-                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-jetty-adapter "1.9.5"]
                  [ring-cors "0.1.13"]]
 
   :plugins [[lein-environ "1.2.0"]]
