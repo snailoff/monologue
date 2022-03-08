c9fd22081d491af6867d9523a42841caa4125468
snailoff <snailoff@minimalworks.io>
2022-03-08 09:19:18 +0900

    [feat] route, view 합침 (route missing, 순환참조 문제)

--- knot/frontend/app.cljs
+++ knot/frontend/app.cljs
@@ -1,27 +1,74 @@
 (ns knot.frontend.app
-  (:require [reagent.core :as r]
+  (:require [reagent.core :as reagent]
             [reitit.frontend :as rf]
             [reitit.frontend.easy :as rfe]
             [reitit.coercion.spec :as rss]
             [reagent.dom :as rdom]
             [fipp.edn :as fedn]
-            [knot.frontend.views :as views]
-            [knot.frontend.state :refer [app-state]]))
+            [knot.frontend.state :refer [app-state]]
+            [knot.frontend.actions :as action]))
 
-(defonce match (r/atom nil))
+(defonce match (reagent/atom nil))
+
+
+(defn piece-list-component []
+      (reagent/create-class
+        {:display-name "piece list"
+         :component-did-mount (fn [this]
+                                  (action/get-pieces))
+         :reagent-render (fn [this]
+                             [:div.content
+                              [:h3.title "piece list"]
+                              [:ul
+                               (for [piece (@app-state :pieces)]
+                                    ^{:key piece}
+                                    [:li [:a {:href (rfe/href ::piece-one {:piece-id (piece :id)})} (piece :subject)]])]])}))
+
+
+(defn piece-one-component [match]
+      (let [{:keys [path]} (:parameters match)
+            {:keys [piece-id]} path]
+           (reagent/create-class
+             {:display-name "piece one"
+              :component-did-mount (fn [this]
+                                       (action/get-piece piece-id))
+              :reagent-render (fn [this]
+                                  (let [piece (@app-state :piece)]
+                                       [:div
+                                        [:h3.title (piece :subject)]
+                                        [:h5.subtitle (piece :summary)]
+                                        [:div.content (piece :content)]]))})))
+
+
+(defn piece-recent-one-component []
+      (reagent/create-class
+        {:display-name "piece recent one"
+         :component-did-mount (fn [this]
+                                  (action/get-piece-recent-one))
+
+         :reagent-render (fn [this]
+                             [:div.content
+                              [:h3.title "piece recent one"]
+                              [:ul
+                               (let [piece (get @app-state :piece)]
+                                    [:li (piece :subject)])]])}))
+
 
 (def routes
   [["/piece-list" {:name ::piece-list
-                   :view views/piece-list-component}]
-   ["/piece-one" {:name ::piece-one
-                  :view views/piece-recent-one-component}]])
-
+                   :view piece-list-component}]
+   ["/piece-recent" {:name ::piece-recent
+                           :view piece-recent-one-component}]
+   ["/piece-one/:piece-id" {:name ::piece-one
+                            :path {:piece-id int?}
+                            :view piece-one-component}]])
 
 (defn current-page []
       [:div
        [:ul
         [:li [:a {:href (rfe/href ::piece-list)} "list"]]
-        [:li [:a {:href (rfe/href ::piece-one)} "one"]]]
+        [:li [:a {:href (rfe/href ::piece-recent)} "recent"]]
+        [:li [:a {:href (rfe/href ::piece-one {:piece-id 85})} "piece"]]]
        (if @match
          (let [view (:view (:data @match))]
               [view @match]))
