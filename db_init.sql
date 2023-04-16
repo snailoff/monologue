b2bb70b0df3d4c5c47556a12b36a55d51d3aa453
snailoff <snailoff@gmail.com>
2023-04-16 09:35:55 +0900

    [feature] db schem

--- db_init.sql
+++ db_init.sql
@@ -7,7 +7,7 @@ CREATE TABLE "public"."knot_meta" (
 
 
 CREATE TABLE "public"."knot_pieces" (
-    "id" serial,
+    "id" bpchar(15) NOT NULL,
     "subject" varchar NOT NULL,
     "summary" varchar,
     "content" text,
@@ -15,6 +15,9 @@ CREATE TABLE "public"."knot_pieces" (
     "mtime" timestamp NOT NULL DEFAULT now(),
     PRIMARY KEY ("id")
 );
+
+CREATE UNIQUE INDEX piece_u_subject
+ON knot_pieces(subject);
 
 CREATE TABLE "public"."knot_tags" (
     "id" serial,
@@ -25,16 +28,19 @@ CREATE TABLE "public"."knot_tags" (
     PRIMARY KEY ("id")
 );
 
+CREATE UNIQUE INDEX tag_u_name
+ON knot_tags(name);
+
 CREATE TABLE "public"."link_pieces" (
-    "from_piece_id" int4 NOT NULL,
-    "to_piece_id" int4 NOT NULL,
+    "from_piece_id" bpchar(15) NOT NULL,
+    "to_piece_id" bpchar(15) NOT NULL,
     "ctime" timestamp NOT NULL DEFAULT now(),
     PRIMARY KEY ("from_piece_id","to_piece_id")
 );
 
 CREATE TABLE "public"."link_tag_piece" (
     "tag_id" int4 NOT NULL,
-    "piece_id" int4 NOT NULL,
+    "piece_id" bpchar(15) NOT NULL,
     "ctime" timestamp NOT NULL DEFAULT now(),
     PRIMARY KEY ("tag_id","piece_id")
 );
