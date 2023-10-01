f6931db050d02e162d13fbe5af4f73199359f4ea
snail <snailoff@gmail.com>
2023-10-01 14:47:05 +0900

    [feature] parameter middleware

--- src/monologue/backend/router.clj
+++ src/monologue/backend/router.clj
@@ -3,12 +3,11 @@
             [muuntaja.core :as muun]
             [reitit.ring :as ring]
             [reitit.ring.coercion :as rrc]
-            [reitit.interceptor.sieppari]
-            [reitit.coercion.schema]
-            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.middleware.muuntaja :as rrm-muuntaja]
+            [reitit.ring.middleware.parameters :as rrm-parameter]
+            [reitit.coercion.spec]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.cors :refer [wrap-cors]]
-            [schema.core :as s]
             [taoensso.timbre :as b]))
 
 
@@ -16,28 +15,33 @@
   (ring/ring-handler
     (ring/router
       [["/api"
-        ["/piece-recent-list" {:get {:coercion   reitit.coercion.schema/coercion
-                                     :parameters {}
+        ["/piece-recent-list" {:get {:parameters {}
                                      :responses  {200 {}}
                                      :handler    (fn [{:keys []}]
                                                    {:status 200
                                                     :body   {:pieces (mapper/pieces-recent 10)}})}}]
-        ["/piece-recent-one" {:get {:coercion   reitit.coercion.schema/coercion
-                                    :parameters {}
+        ["/piece-recent-one" {:get {:parameters {}
                                     :responses  {200 {}}
                                     :handler    (fn [{:keys []}]
                                                   {:status 200
                                                    :body   {:piece (mapper/pieces-recent-one)}})}}]
-        ["/piece/:piece-id" {:get {:coercion   reitit.coercion.schema/coercion
-                                   :parameters {:path {:piece-id s/Str}}
+        ["/piece/:piece-id" {:get {:parameters {:path {:piece-id string?}}
                                    :responses  {200 {:piece {}}}
                                    :handler    (fn [{:keys [parameters]}]
                                                  {:status 200
-                                                  :body   {:piece (mapper/pieces-one (-> parameters :path :piece-id))}})}}]]]
-
-
-      {:data {:muuntaja   muun/instance
-              :middleware [muuntaja/format-middleware
+                                                  :body   {:piece (mapper/pieces-one (-> parameters :path :piece-id))}})}}]]
+       ["/blog"
+        ["/tistory/auth-code-callback" {:get {:parameters {:query {:code  string?
+                                                                   :state string?}}
+                                              :responses  {200 {}}
+                                              :handler    (fn [{{{:keys [code state]} :query} :parameters}]
+                                                            {:status 200
+                                                             :body   {:authorization-code code
+                                                                      :state              state}})}}]]]
+      {:data {:coercion   reitit.coercion.spec/coercion
+              :muuntaja   muun/instance
+              :middleware [rrm-muuntaja/format-middleware
+                           rrm-parameter/parameters-middleware
                            rrc/coerce-exceptions-middleware
                            rrc/coerce-request-middleware
                            rrc/coerce-response-middleware]}})
@@ -48,7 +52,7 @@
 (def app
   (wrap-cors app-route
              :access-control-allow-origin [#".*"]
-             :access-control-allow-methods [:get]))
+             :access-control-allow-methods [:get :options]))
 
 (defn start []
   (jetty/run-jetty app {:port 1234, :join? false})
